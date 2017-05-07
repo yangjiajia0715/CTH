cfa766579be2a5c12e439dd83da772c9e7e5580f
yangjiajiaCompany <875880715@qq.com>
2017-05-07 22:53:25 +0800

    style

--- app/src/main/java/com/cth/cuotiben/activity/BaseActivity.java
+++ app/src/main/java/com/cth/cuotiben/activity/BaseActivity.java
@@ -3,7 +3,13 @@ package com.cth.cuotiben.activity;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.cth.cuotiben.R;
+
+import butterknife.ButterKnife;
 
 /**
  * BaseActivity
@@ -12,12 +18,31 @@ import android.support.v7.app.AppCompatActivity;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
+    private Toolbar mToolBar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getLayoutId());
+        ButterKnife.bind(this);
+
+        mToolBar = (Toolbar) findViewById(R.id.toolbar);
+        if (mToolBar != null) {
+            setSupportActionBar(mToolBar);
+            initToolBar(mToolBar);
+        }
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowTitleEnabled(false);
+        }
+
         initView();
         initData();
+    }
+
+    private void initToolBar(Toolbar mToolBar) {
+
     }
 
     @LayoutRes
--- app/src/main/java/com/cth/cuotiben/activity/MainActivity.java
+++ app/src/main/java/com/cth/cuotiben/activity/MainActivity.java
@@ -1,15 +1,36 @@
 package com.cth.cuotiben.activity;
 
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
 import com.cth.cuotiben.R;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
 import io.reactivex.Observable;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.functions.Consumer;
 
 public class MainActivity extends BaseActivity {
+    private static final String TAG = "MainActivity";
+    @BindView(R.id.bottom_navi_home_page)
+    RadioButton bottomNaviHomePage;
+    @BindView(R.id.bottom_navi_answer)
+    RadioButton bottomNaviAnswer;
+    @BindView(R.id.bottom_navi_create_wrong_topic)
+    RadioButton bottomNaviCreateWrongTopic;
+    @BindView(R.id.bottom_navi_chat)
+    RadioButton bottomNaviChat;
+    @BindView(R.id.bottom_navi_my)
+    RadioButton bottomNaviMy;
+    @BindView(R.id.bottom_navi_layout)
+    RadioGroup bottomNaviLayout;
 
     @Override
     protected int getLayoutId() {
@@ -30,18 +51,40 @@ public class MainActivity extends BaseActivity {
                 .doOnNext(new Consumer<String>() {
                     @Override
                     public void accept(@NonNull String s) throws Exception {
-
+                        Log.d(TAG, "doOnNext---accept: " + s);
+                        if ("hhh".equals(s)) {
+                            s = "hhh1";
+                        }
                     }
                 })
                 .subscribe(new Consumer<String>() {
                     @Override
                     public void accept(@NonNull String s) throws Exception {
-
+                        Log.d(TAG, "accept: sssssssss=" + s);
                     }
                 });
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_home:
+                Toast.makeText(this, "HHHHH", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    @Override
     protected void initData() {
     }
+
 }
