ca18d3976ad8e15dffbeb4ad8697c1e0a1302d88
yangjiajiaCompany <875880715@qq.com>
2017-05-07 21:08:26 +0800

    rxjava

--- app/src/main/java/com/cth/cuotiben/activity/MainActivity.java
+++ app/src/main/java/com/cth/cuotiben/activity/MainActivity.java
@@ -2,6 +2,13 @@ package com.cth.cuotiben.activity;
 
 import com.cth.cuotiben.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
+
 public class MainActivity extends BaseActivity {
 
     @Override
@@ -11,7 +18,27 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initView() {
+        List<String> strings = new ArrayList<>();
+        strings.add("hhh");
+        strings.add("hhh2");
+        strings.add("hhh3");
 
+        String[] strs = new String[strings.size()];
+        strings.toArray(strs);
+
+        Observable.fromIterable(strings)
+                .doOnNext(new Consumer<String>() {
+                    @Override
+                    public void accept(@NonNull String s) throws Exception {
+
+                    }
+                })
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(@NonNull String s) throws Exception {
+
+                    }
+                });
     }
 
     @Override
